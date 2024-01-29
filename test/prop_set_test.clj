0597bc74ad83c9f9f416b983dc9242fb65fc5c5f
Holdest White <zbsholdest@mail.ru>
2024-01-29 21:27:46 +0300

    fmt

--- test/prop_set_test.clj
+++ test/prop_set_test.clj
@@ -8,39 +8,31 @@
 (defn add-list [list collection]
   (cond
     (= (count list) 0) [nil]
-    :else (reduce (fn [accum val] (set/add val accum)) collection list)
-    )
-  )
+    :else (reduce (fn [accum val] (set/add val accum)) collection list)))
 
 (defn delete-list [list collection] (reduce (fn [accum val] (set/delete val accum)) collection list))
 
 (defspec set-add 100
   (prop/for-all [v (gen/vector (gen/one-of [gen/large-integer gen/small-integer]))]
-    (= (sort (set/get-vector (add-list v nil))) (sort (vec (set v))))))
+                (= (sort (set/get-vector (add-list v nil))) (sort (vec (set v))))))
 
 (defspec set-delete 100
   (prop/for-all [v (gen/vector (gen/one-of [gen/boolean gen/large-integer gen/double gen/string-alphanumeric]))]
-    (=
-      (vec (set (delete-list v (add-list v nil))))
-      [nil]
-      )))
+                (=
+                 (vec (set (delete-list v (add-list v nil))))
+                 [nil])))
 
 (defspec set-reduce 100
   (prop/for-all [v (gen/vector (gen/one-of [gen/boolean gen/large-integer gen/double gen/string-alphanumeric]))]
-    (=
-      (sort
-        (set/reduce-set
-          (fn [accum val] (conj accum (hash val)))
-          '()
-          (add-list v nil)
-          )
-        )
+                (=
+                 (sort
+                  (set/reduce-set
+                   (fn [accum val] (conj accum (hash val)))
+                   '()
+                   (add-list v nil)))
 
-      (sort
-        (reduce
-          (fn [accum val] (conj accum (hash val)))
-          '()
-          (set v)
-          )
-        )
-      )))
+                 (sort
+                  (reduce
+                   (fn [accum val] (conj accum (hash val)))
+                   '()
+                   (set v))))))
